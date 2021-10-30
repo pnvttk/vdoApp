59ac63cc796f27a2286a365836d94546b698f315
G0lDenSeal <pannavach@outlook.com>
2021-10-30 13:21:05 +0700

    p11

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const Video = require('../models/video')
 
 const db = "mongodb://127.0.0.1:27017/meanauth"
 mongoose.Promise = global.Promise
@@ -10,8 +11,28 @@ mongoose.connect(db, function (err) {
     }
 })
 
-router.get('/', function (req, res) {
-    res.send('api works')
+router.get('/videos', function (req, res) {
+    console.log('Get request for all videos')
+    Video.find({})
+        .exec(function (err, videos) {
+            if (err) {
+            console.log("Error retrieving videos")
+            } else {
+                res.json(videos)
+        }
+    })
+})
+
+router.get('/videos/:id', function (req, res) {
+    console.log('Get request for one videos')
+    Video.findById(req.params.id)
+        .exec(function (err, videos) {
+            if (err) {
+            console.log("Error retrieving videos")
+            } else {
+                res.json(videos)
+        }
+    })
 })
 
 module.exports = router;
