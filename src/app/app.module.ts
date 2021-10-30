b88717ad5ae1ed2e2c4bea58966d1094c910de56
G0lDenSeal <pannavach@outlook.com>
2021-10-30 14:45:10 +0700

    p15 angular link home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideoCenterComponent
   ],
   imports: [
     BrowserModule,
